a6e5e4e5d14404a3973c1b8d276c449fe2321db1
Yogesh Kaushik <masterlukeskywalker02@gmail.com>
2022-01-10 01:18:35 +0530

    merger file

--- DataSets/merge.py
+++ DataSets/merge.py
@@ -1,5 +1,17 @@
 import pandas as pd
 import os
 
-area_prod_df=pd.read_csv(os.getcwd())
-fert_df=pd.read_csv("")
+area_prod_df = pd.read_csv(os.getcwd()+"/dt_area_prod_a_web.csv")
+fert_df = pd.read_csv(os.getcwd()+"/dt_fert_consumption_a_web.csv")
+rainfall_df = pd.read_csv(os.getcwd()+"/dt_normal_rainfall_a_web.csv")
+soil_df = pd.read_csv(os.getcwd()+"/dt_soil_type_a_web.csv")
+
+print(area_prod_df.columns)
+print(fert_df.columns)
+print(rainfall_df.columns)
+print(soil_df.columns)
+
+area_fert = pd.merge(area_prod_df, fert_df, on=['YEAR'])
+print(area_fert.columns)
+print(area_fert.head)
+
