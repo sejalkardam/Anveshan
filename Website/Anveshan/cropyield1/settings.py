97b0b2f593bc7fe044b89ba2c1250006e24642b0
sejalkardam <sejal20467@iiitd.ac.in>
2022-01-10 18:19:34 +0530

    heroku

--- Website/Anveshan/cropyield1/settings.py
+++ Website/Anveshan/cropyield1/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from . info import *
+import django_heroku
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -130,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
