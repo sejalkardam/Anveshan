4cf22ef31f8762096457d11f64c9db639c4fdd46
sejalkardam <sejal20467@iiitd.ac.in>
2022-01-10 20:47:26 +0530

    updates

--- cropyield1/settings.py
+++ cropyield1/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from . info import *
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -84,12 +85,15 @@ WSGI_APPLICATION = 'cropyield1.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = dict()
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=False)
 
 
 # Password validation
